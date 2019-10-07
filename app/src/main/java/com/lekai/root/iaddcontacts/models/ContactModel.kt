a168d949522cee8db37f696501323ef53f1ec5b4
Khalidtoak <kaleed4allah@yahoo.com>
2019-10-07 03:11:52 +0100

    changed contact model to data class

--- app/src/main/java/com/lekai/root/iaddcontacts/models/ContactModel.kt
+++ app/src/main/java/com/lekai/root/iaddcontacts/models/ContactModel.kt
@@ -4,4 +4,4 @@ package com.lekai.root.iaddcontacts.models
  * Created by root on 4/21/17.
  */
 
-class ContactModel(var name: String?, var phone: String?)
+data class ContactModel(var name: String?, var phone: String?)
